f551876b8ce446dfbc2c93d2368871d3295e1a02
5huv0 <md88748874@gmail.com>
2024-05-30 02:30:26 +0600

    added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import MainSection from './components/MainSection';
+import React , { useState } from 'react';
 
 function App() {
+  const[dark, setDark] = useState('light');
+  const[green, setGreen] = useState('light');
+  const[blue, setBlue] = useState('light');
+  const[grey, setGrey] = useState('light');
+
+
+  const showDark =()=> {
+    if(dark === 'light'){
+      setDark('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setDark('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  } // This is for Dark mode
+
+  const showGreen =()=> {
+    if(green === 'light'){
+      setDark('green');
+      document.body.style.backgroundColor = 'green';
+    }
+    else{
+      setDark('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  } // This is for Green mode
+
+  const showBlue =()=> {
+    if(blue === 'light'){
+      setDark('blue');
+      document.body.style.backgroundColor = 'blue';
+    }
+    else{
+      setDark('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  } // This is for Blue mode
+
+  const showGrey =()=> {
+    if(grey === 'light'){
+      setDark('grey');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setDark('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  } // This is for Grey mode
+
   return (
     <>
-    <Navbar name ='Any Heading Here'/>
+    <Navbar name ='Any Heading Here' dark = {dark} showDark = {showDark} green = {green} showGreen = {showGreen} blue = {blue} showBlue = {showBlue} grey = {grey} showGrey = {showGrey}/>
     <div className="coantainer my-5">
        <MainSection/>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
+
 export default function Navbar(props) {
   return (
-  <nav className="navbar bg-body-tertiary container">
+  <nav className={`navbar bg-body-${props.showDark} container`}>
     <div className="container-fluid">
       <span className="navbar-brand mb-0 h1">{props.name}</span>
         <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label" for="flexSwitchCheckDefault">Toggle Switch</label>
+            <input className="form-check-input" onClick={props.showDark} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark</label>
+        </div>
+        {/* <div className="form-check form-switch">
+            <input className="form-check-input" onClick={} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Light</label>
+        </div> */}
+        <div className="form-check form-switch">
+            <input className="form-check-input" onClick={props.showGreen} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Green</label>
+        </div>
+        <div className="form-check form-switch">
+            <input className="form-check-input" onClick={props.showBlue} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Blue</label>
+        </div>
+        <div className="form-check form-switch">
+            <input className="form-check-input" onClick={props.showGrey} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Grey</label>
         </div>
     </div>
   </nav>
